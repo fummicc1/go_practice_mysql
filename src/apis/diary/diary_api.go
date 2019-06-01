d8a67afa2d5573c9be61c9f0ea71d96bfbfec4fa
fummicc1 <fumiya.tennis1@gmail.com>
2019-06-01 23:24:53 +0900

    fix models. add api

---
+++ src/apis/diary/diary_api.go
@@ -0,0 +1,29 @@
+package product
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/fummicc1/diary_api_go/src/config"
+	"github.com/fummicc1/diary_api_go/src/models"
+)
+
+func FindAll(response http.ResponseWriter, request *http.Request) {
+	db, err := config.GetDB()
+	if err != nil {
+		respondWithError(response, http.StatusBadRequest, err.Error())
+	} else {
+		diaryModel := models.ProductModel{}
+	}
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJson(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
--- src/models/diary_model.go
+++ src/models/diary_model.go
@@ -2,12 +2,38 @@ package models
 
 import (
 	"database/sql"
+
+	"github.com/fummicc1/diary_api_go/src/entitles"
 )
 
 type DiaryModel struct {
 	DB *sql.DB
 }
 
-func (diaryModel DiaryModel) FindAll() (diary []DiaryModel, err error) {
+func (diaryModel DiaryModel) FindAll() (diary []entitles.Diary, err error) {
 	rows, err := diaryModel.DB.Query("select * from diary")
+	if err != nil {
+		return nil, err
+	} else {
+		var diaries []entitles.Diary
+		for rows.Next() {
+			var id int64
+			var sender string
+			var title string
+			var content string
+			err2 := rows.Scan(&id, &sender, &title, &content)
+			if err2 != nil {
+				return nil, err2
+			} else {
+				diary := entitles.Diary{
+					Id:      id,
+					Sender:  sender,
+					Title:   title,
+					Content: content,
+				}
+				diaries = append(diaries, diary)
+			}
+		}
+		return diaries, nil
+	}
 }
