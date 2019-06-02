668c84c658b193721350f011b00272ee384c1c0e
fummicc1 <fumiya.tennis1@gmail.com>
2019-06-02 16:44:17 +0900

    client done

--- SimpleDiaryClient/SimpleDiaryClient/ViewController.swift
+++ SimpleDiaryClient/SimpleDiaryClient/ViewController.swift
@@ -1,18 +1,27 @@
-//
-//  ViewController.swift
-//  SimpleDiaryClient
-//
-//  Created by Fumiya Tanaka on 2019/06/02.
-//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
-//
-
 import UIKit
+
+struct Diary: Codable {
+    let id: Int
+    let sender: String
+    let title: String
+    let content: String
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = URL(string: "http://localhost:5000/api/diary/findall")!
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                fatalError("\(error)")
+            }
+            if let data = data, let diaries = try? JSONDecoder().decode([Diary].self, from: data) {
+                for diary in diaries {
+                    print(diary)
+                }
+            }
+        }.resume()
     }
 
 
