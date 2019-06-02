52aa60cdf6ae0df6980cf4e2f65ecf8dd24fc326
fummicc1 <fumiya.tennis1@gmail.com>
2019-06-02 16:34:19 +0900

    addClient

---
+++ SimpleDiaryClient/SimpleDiaryClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleDiaryClient
+//
+//  Created by Fumiya Tanaka on 2019/06/02.
+//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
